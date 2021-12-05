4148e3637cc7a2917e8db160d5c77fc6a46bc91d
fosabdul <um0364nu@go.minnstate.edu>
2021-12-05 07:18:47 -0600

    done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id='app'>
+   <New-Student-Form v-on:student-added="newStudentAdded"></New-Student-Form>
+    <Student-Table
+      v-bind:students="students" v-on:student-arrived-or-left="studentArrivedOrLeft"
+      v-on:delete-student="studentDeleted">
+    </Student-Table>
+    <StudentMessage v-bind:student="mostRecentStudent"></StudentMessage> 
+    <!-- name of the prop = student 
+    name of the data = mostRecentStudent -->
+
+
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  
+    components: {
+          NewStudentForm,
+          StudentMessage,
+          StudentTable,
+
+    },
+  
+  data(){
+    return{
+      students: [],
+      mostRecentStudent: {}  // to modify StudentArrivedorLeft method
+    }
+  
+},
+
+  methods: {
+    newStudentAdded(student){
+      this.students.push(student)
+      this.students.sort(function(s1, s2){
+        return s1.name.toLowCase() < s2.name.toLowCase() ? -1 : 1
+      })
+    },
+    studentArriveOrLeft(student, present){
+    // find student in array of students
+    //update present attribute
+
+      let updateStudent = this.students.find( function(s){ // it will find the matching student or the first matching one / s = every student in the array
+      if (s.name === student.name && s.startID === student.startID){
+          return true
+      }
+    })
+
+    if(updateStudent){ 
+      updateStudent.present = present
+      this.mostRecentStudent = student // this will tell msg to studentMessage the most recent student
+    }
+  },
+  studentDeleted(student){
+    // fileter returns a new array of all students for whom the function returns true
+    this.students = this.students.filter(function (s){
+      // this is the student to update
+      if (s != student){
+        return true
+       
+      }
+      // 
+
+    })
+    // to clear the welcome msg after the student is deleted 
+    this.mostRecentStudent = {}
+
+
   }
 }
+
+}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style> @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+
+
+
+
 </style>
